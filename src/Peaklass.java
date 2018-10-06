373bea7b1e5802ce75911782814076de5ef74324
Karin <karinkruuse98@gmail.com>
2018-10-06 15:00:47 +0300

    testManager() method that might be useful, for test adding a new manager should be added

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class Peaklass {
+
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String args[]) {
+
+        testManager();
+
+    }
+
+    public static void testManager() {
+
+        String[] testid = {"[0] Lõpeta!", "[1]", "[2]"};
+
+        System.out.println("Millist testi soovite teha?");
+        for (String elem: testid) {
+            System.out.println(elem);
+        }
+        int test = s.nextInt();
+
+        while(test != 0) {
+            test = s.nextInt();
+            System.out.println("Millist testi soovite teha?");
+        }
+
+    }
+
+
 }
