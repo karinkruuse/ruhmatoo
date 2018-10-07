a3c3b8a6d53296fdb6f9f71cca6d9694917f2832
karinkruuse <karinkruuse98@gmail.com>
2018-10-07 16:27:14 +0300

    Update Test.java
    MUutsin Test() klassi

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,7 @@
 import java.io.File;
-
 public interface Test {
 
-    int küsi(String küsimus);  // Küsib küsimuse (parameeter), tagastab vastused nr vms
-    int küsi(File fail, int küsimuseNr);
-
-
+    int küsiVastus(String küsimus);
+    String[] loeKüsimused(File fail, int küsimuseNr);
+    
 }
